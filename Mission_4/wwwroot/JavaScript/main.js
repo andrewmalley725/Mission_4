7646af76cd9d854103db352c170382a3d88802f8
Andrew Alley <andrew.alley99@gmail.com>
2023-01-25 22:09:04 -0700

    Add project files.

---
+++ Mission_4/wwwroot/JavaScript/main.js
@@ -0,0 +1,28 @@
+﻿
+
+function calGrade() {
+    let assignScore = $('#assign').val() * .5;
+    let groupScore = $('#group').val() * .1;
+    let quizScore = $('#quiz').val() * .1;
+    let midScore = $('#mid').val() * .1;
+    let finalScore = $('#final').val() * .1;
+    let intexScore = $('#intex').val() * .1;
+
+    let total = assignScore + groupScore + quizScore + midScore + finalScore + intexScore;
+    total = Math.round(total);
+
+    if (total == 0) {
+        alert('Please enter valid numbers!');
+    }
+
+    else {
+        document.getElementById('total').innerHTML += (total + '%');
+        document.getElementById('total').style.display = 'block';
+    }
+    
+}
+
+function hideScore() {
+    document.getElementById("total").style.display = 'none';
+    document.getElementById("total").innerHTML = 'Final score: ';
+}
