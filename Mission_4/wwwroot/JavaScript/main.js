6a6189e5c19ae5531059f1654fe9bbc4f825bcec
Andrew Alley <andrew.alley99@gmail.com>
2023-01-28 10:48:51 -0700

    finished

--- Mission_4/wwwroot/JavaScript/main.js
+++ Mission_4/wwwroot/JavaScript/main.js
@@ -9,20 +9,20 @@ function calGrade() {
     let intexScore = $('#intex').val() * .1;
 
     let total = assignScore + groupScore + quizScore + midScore + finalScore + intexScore;
-    total = Math.round(total);
+    total = Math.round(total); 
 
-    if (total == 0) {
+    if (total == 0) { //if total is 0: re-prompt to enter numbers with value
         alert('Please enter valid numbers!');
     }
 
     else {
-        document.getElementById('total').innerHTML += (total + '%');
-        document.getElementById('total').style.display = 'block';
+        document.getElementById('total').innerHTML += (total + '%'); //adds toatal to innerHTML of current total tag
+        document.getElementById('total').style.display = 'block'; //unhides p-tag with total percentage
     }
     
 }
 
-function hideScore() {
+function hideScore() { //hides total score when clear button is clicked
     document.getElementById("total").style.display = 'none';
     document.getElementById("total").innerHTML = 'Final score: ';
 }
